ab3b65e17c23048db0ff389a6556aa4aaff12f8d
Benjamin Fildier <benjamin.fildier@gmail.com>
2017-08-29 17:58:54 -0700

    add physical and thermo constants

---
+++ tests/test_physicalConstants.py
@@ -0,0 +1,17 @@
+import sys,os
+import numpy as np
+
+## Own functions
+currentpath = os.path.dirname(os.path.realpath(__file__))
+sys.path.insert(0,os.path.join(os.path.dirname(currentpath),'functions'))
+
+from physicalConstants import *
+
+if __name__ == "__main__":
+
+	vars = locals().copy()
+	n = max(map(len,vars))
+	print("Variables in the local environment:")
+	for var in vars.keys():
+
+		print(('{:%ds} :'%n).format(var), vars[var])
