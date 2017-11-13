d078db6f6bd82531a45eea93832cfd714666c948
Benjamin Fildier <benjamin.fildier@gmail.com>
2017-11-12 19:48:29 -0800

    script to run analysis on compute nodes

--- scripts/ipynb2py36.sh
+++ scripts/ipynb2py36.sh
@@ -4,17 +4,18 @@ inputipynb=$1
 outputname=$2
 
 # Load python environment
-source activate py36
+#source activate py36
+module load python/3.6-anaconda-4.4
 
 # Copy jupyter notebook in temp file
 jupyter nbconvert --to notebook $inputipynb --output temp
 # Comment instances of all jupyter magics
-sed -i '' 's/%%time/#%%time/g' temp.ipynb
-sed -i '' 's/%load_ext/#%load_ext/g' temp.ipynb
-sed -i '' 's/%matplotlib/#%matplotlib/g' temp.ipynb
-sed -i '' 's/%autoreload/#%autoreload/g' temp.ipynb
+sed -i='' 's/%%time/#%%time/g' temp.ipynb
+sed -i='' 's/%load_ext/#%load_ext/g' temp.ipynb
+sed -i='' 's/%matplotlib/#%matplotlib/g' temp.ipynb
+sed -i='' 's/%autoreload/#%autoreload/g' temp.ipynb
 # Comment figure display
-sed -i '' 's/plt.show()/plt.close()/g' temp.ipynb
+sed -i='' 's/plt.show()/plt.close()/g' temp.ipynb
 
 # Convert temp copy to python script
 jupyter nbconvert --to script temp.ipynb --output $outputname
