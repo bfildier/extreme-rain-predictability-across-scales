afe1ed65a68a21a4a40af7b889612f4ec856e1b1
Benjamin Fildier <benjamin.fildier@gmail.com>
2017-11-17 18:03:07 -0800

    update analysisScalingAllScales.sh

--- scripts/analyzeScalingAllScales.sh
+++ scripts/analyzeScalingAllScales.sh
@@ -2,7 +2,7 @@
 
 module load python/3.6-anaconda-4.4
 
-## Directory to store all scripts
+##-- Set working directory --##
 SCRIPTDIR=automated_scripts
 if [ ! -d "$SCRIPTDIR" ]
 then
@@ -10,21 +10,41 @@ then
     mkdir $SCRIPTDIR/logs
 fi 
 
-## Run options
+##-- Run options--##
 todaysdate=`date +"%Y%m%d-%H%M"`
+template_nameroot=analyzeScaling_template_$todaysdate
+template_analysis_script=${template_nameroot}.py
+template_batch_script=sbatch_${template_nameroot}.sbatch
 
-# Edit options in script
+##-- Analysis script options --##
 startdate=185005010100
-enddate=185005020000
-#enddate=185005020000
+enddate=185105010000
 daskarray=False
 tracktime=True
 compsets='FSPCAMm_AMIP'
 experiments='piControl'
 time_strides='1hr'
-resolutions='dx'
+resolutions='3dx'
 
 
+##-- Batch script options --##
+# WRITE HERE SBATCH OPTIONS
+
+##-- Main --##
+
+# Template scripts
+./ipynb2py36.sh analyzePointWiseScalingOmega500TsPs_template.ipynb $SCRIPTDIR/${template_nameroot}
+cp sbatch_template_analyzePointWiseScalingOmega500TsPs.sbatch $SCRIPTDIR/${template_batch_script}
+
+cd $SCRIPTDIR
+
+# Correct paths and change global options in analysis script
+sed -i'' 's/(workdir)/(os.path.dirname(workdir))/g' ${template_analysis_script}
+sed -i'' 's/^daskarray =.*/daskarray = '${daskarray}'/' ${template_analysis_script}
+sed -i'' 's/^tracktime =.*/tracktime = '${tracktime}'/' ${template_analysis_script}
+
+
+# Launch all runs
 for compset in `echo ${compsets}`;
 do
     for experiment in `echo ${experiments}`;
@@ -36,38 +56,36 @@ do
 
 ##-- Create scripts --##
                  
-                nameroot=analyzePointWiseScalingOmega500TsPs_${todaysdate}_${compset}_${experiment}_${time_stride}_${resolution}
-                analysisscript=${SCRIPTDIR}/${nameroot}.py
-                batchscript=${SCRIPTDIR}/sbatch_${nameroot}.sbatch
+                nameroot=${template_nameroot}_${compset}_${experiment}_${time_stride}_${resolution}
+                analysisscript=${nameroot}.py
+                batchscript=sbatch_${nameroot}.sbatch
 
-                # Convert notebook to python script
-                ./ipynb2py36.sh analyzePointWiseScalingOmega500TsPs_template.ipynb ${SCRIPTDIR}/${nameroot}
+                # Duplicate analysis script
+                cp ${template_analysis_script} ${analysisscript}
                 # Duplicate batch script
-                cp sbatch_template_analyzePointWiseScalingOmega500TsPs.sbatch $batchscript
+                cp ${template_batch_script} $batchscript
 
-##-- Python script options --#
+##-- Analysis script options --#
 
-                sed -i='' 's/^dates =.*/dates = "'$startdate'","'$enddate'"/g' ${analysisscript}
-                sed -i='' 's/^daskarray =.*/daskarray = '${daskarray}'/' ${analysisscript}
-                sed -i='' 's/^tracktime =.*/tracktime = '${tracktime}'/' ${analysisscript}
-                sed -i='' 's/^compset =.*/compset = "'${compset}'"/' ${analysisscript}
-                sed -i='' 's/^experiment =.*/experiment = "'${experiment}'"/' ${analysisscript}
-                sed -i='' 's/^time_stride =.*/time_stride = "'${time_stride}'"/' ${analysisscript}
-                sed -i='' 's/^resolution =.*/resolution = "'${resolution}'"/' ${analysisscript}
+                sed -i'' 's/^dates =.*/dates = "'$startdate'","'$enddate'"/' ${analysisscript}
+                sed -i'' 's/^compset =.*/compset = "'${compset}'"/' ${analysisscript}
+                sed -i'' 's/^experiment =.*/experiment = "'${experiment}'"/' ${analysisscript}
+                sed -i'' 's/^time_stride =.*/time_stride = "'${time_stride}'"/' ${analysisscript}
+                sed -i'' 's/^resolution =.*/resolution = "'${resolution}'"/' ${analysisscript}
                 
 ##-- Batch script options --#
                 
-                sed -i='' 's/^#SBATCH --output=.*/#SBATCH --output="../logs/'${scriptname}'.%j.%N.out"/' $batchscript
-                sed -i='' 's/^scriptname=.*/scriptname='${nameroot}'/' $batchscript
+                sed -i'' 's/^#SBATCH --output=.*/#SBATCH --output="logs\/'${nameroot}'.%j.%N.out"/' $batchscript
+                sed -i'' 's/^scriptname=.*/scriptname='${nameroot}'/' $batchscript
 
-##-- Send batch script to slurm --##
+##-- Launch batch script--##
 
                 sbatch $batchscript
-                echo $batchscript" submitted"
                 
             done
         done
     done
 done
+
 
 exit 0
